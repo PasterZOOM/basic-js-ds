929c2bdf298bc01d8b249704f3e7df16bda244f9
ilya <pasterzoom@gmail.com>
2023-04-12 23:05:34 +0300

    feat: made a queue and stack

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,23 +14,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+    queue = {}
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    getUnderlyingList() {
+        return this.queue
+    }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    enqueue(value) {
+        if (typeof this.queue.next === 'undefined') {
+            this.queue = {value, next: null}
+            return this.queue.value
+        }
+        const add = (head, value) => {
+            if (head === null) {
+                return {value, next: null}
+            } else {
+                head.next = add(head.next, value)
+                return head
+            }
+        }
+        this.queue.next = add(this.queue.next, value)
+        return value
+    }
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    dequeue() {
+        const value = this.queue.value
+        this.queue = this.queue.next
+        return value
+    }
 }
 
 module.exports = {
-  Queue
+    Queue
 };
+const queue = new Queue();
+console.log(queue.enqueue(5));
+
+console.log(queue.queue)
+console.log(queue.enqueue(6));
+console.log(queue.queue)
+console.log(queue.enqueue(7));
+console.log(queue.queue)
+
+console.log(queue.dequeue())
